78d381a49fc8685507adbcfe7f451c5c2b86d279
yfla980107 <yfla980107@gmail.com>
2021-04-05 16:13:11 +0900

    210405 Naver/YouTube 영화찾기 앱

--- WpfMiniProject/NaverMovieFinderApp/Helper/Commons.cs
+++ WpfMiniProject/NaverMovieFinderApp/Helper/Commons.cs
@@ -15,6 +15,12 @@ namespace NaverMovieFinderApp
 {
     public class Commons
     {
+        // 즐겨찾기 여부 플래그
+        public static bool IsFavorite = false;
+
+        // 즐겨찾기 삭제와 보기 플래그
+        public static bool IsDelete = false;
+
         // NLog 정적객체
         public static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
 
